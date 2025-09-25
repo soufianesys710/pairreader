6054712608edf592e762de53b00f1c90e50b5509
soufianesys710 <soufianenidam7@gmail.com>
2025-09-25 18:18:15 +0100

    streaming

--- src/pairreader/agents.py
+++ src/pairreader/agents.py
@@ -3,19 +3,23 @@ from pairreader.nodes import QueryOptimizer, InfoRetriever, InfoSummarizer
 from pairreader.vectorestore import VectorStore
 from langgraph.graph.state import StateGraph
 from langgraph.graph import START, END
+from typing import Any
 
 class PairReaderAgent:
     def __init__(self, vs: VectorStore):
         self.vs = vs
-        self.graph_builder = StateGraph(PairReaderState)
-        self.graph_builder.add_node("query_optimizer", QueryOptimizer(query_decomposition=True, query_expansion=True))
-        self.graph_builder.add_node("info_retriever", InfoRetriever(vs))
-        self.graph_builder.add_node("info_summarizer", InfoSummarizer())
-        self.graph_builder.add_edge(START, "query_optimizer")
-        self.graph_builder.add_edge("query_optimizer", "info_retriever")
-        self.graph_builder.add_edge("info_retriever", "info_summarizer")
-        self.graph_builder.add_edge("info_summarizer", END)
-        self.graph = self.graph_builder.compile()
+        self.builder = StateGraph(PairReaderState)
+        self.builder.add_node("query_optimizer", QueryOptimizer(query_decomposition=True, query_expansion=True))
+        self.builder.add_node("info_retriever", InfoRetriever(vs))
+        self.builder.add_node("info_summarizer", InfoSummarizer())
+        self.builder.add_edge(START, "query_optimizer")
+        self.builder.add_edge("query_optimizer", "info_retriever")
+        self.builder.add_edge("info_retriever", "info_summarizer")
+        self.builder.add_edge("info_summarizer", END)
+        self.workflow = self.builder.compile()
 
-    def __call__(self, state: PairReaderState) -> PairReaderState:
-        return self.graph.invoke(state)
+    def invoke(self, state: PairReaderState) -> PairReaderState:
+        return self.workflow.invoke(state)
+    
+    def stream(self, state: PairReaderState, **kwargs) -> Any:
+        return self.workflow.stream(state, **kwargs)
